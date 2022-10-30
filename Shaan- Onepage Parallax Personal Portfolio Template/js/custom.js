cd2aa420c7f8f3307808486bc35f4b5c9f7e48b7
shathibegum1812 <49153354+shathibegum1812@users.noreply.github.com>
2022-10-30 17:38:25 +0600

    Add files via upload

---
+++ Shaan- Onepage Parallax Personal Portfolio Template/js/custom.js
@@ -0,0 +1,215 @@
+/*==============================================================================
+
+    Theme Name: Shaan
+    Theme URI: http://
+    Description: The Multi-Purpose Onepage Template
+    Author: UI-ThemeZ
+    Author URI: http://themeforest.net/user/UI-ThemeZ
+    Version: 1.0
+
+=================================================================================*/
+
+
+$(function() {
+
+    "use strict";
+
+    var wind = $(window);
+
+
+
+    // scrollIt
+    $.scrollIt({
+      upKey: 38,                // key code to navigate to the next section
+      downKey: 40,              // key code to navigate to the previous section
+      easing: 'swing',          // the easing function for animation
+      scrollTime: 600,          // how long (in ms) the animation takes
+      activeClass: 'active',    // class given to the active nav element
+      onPageChange: null,       // function(pageIndex) that is called when page is changed
+      topOffset: -80            // offste (in px) for fixed top navigation
+    });
+
+
+
+    // navbar scrolling background
+    wind.on("scroll",function () {
+
+        var bodyScroll = wind.scrollTop(),
+            navbar = $(".navbar"),
+            logo = $(".navbar .logo> img");
+
+        if(bodyScroll > 100){
+
+            navbar.addClass("nav-scroll");
+            logo.attr('src', 'images/shaan-logo-dark.png');
+
+        }else{
+
+            navbar.removeClass("nav-scroll");
+            logo.attr('src', 'images/shaan-logo-light.png');
+        }
+    });
+
+
+    // close navbar-collapse when a  clicked
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-collapse").removeClass("show");
+    });
+
+    // progress bar
+    wind.on('scroll', function () {
+        $(".skill-progress .progres").each(function () {
+            var bottom_of_object = 
+            $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = 
+            $(window).scrollTop() + $(window).height();
+            var myVal = $(this).attr('data-value');
+            if(bottom_of_window > bottom_of_object) {
+                $(this).css({
+                  width : myVal
+                });
+            }
+        });
+    });
+
+    // sections background image from data background
+    var pageSection = $(".bg-img, section");
+    pageSection.each(function(indx){
+        
+        if ($(this).attr("data-background")){
+            $(this).css("background-image", "url(" + $(this).data("background") + ")");
+        }
+    });
+
+
+    // === owl-carousel === //
+
+    // Testimonials owlCarousel
+    $('.carousel-single .owl-carousel').owlCarousel({
+        items:1,
+        loop:true,
+        margin: 0,
+        mouseDrag:false,
+        autoplay:true,
+        smartSpeed:500,
+        dots: false,
+        nav: true,
+        navText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>']
+    });
+
+    // Clients owlCarousel
+    $('.clients .owl-carousel').owlCarousel({
+        loop:true,
+        margin: 60,
+        mouseDrag:true,
+        dots: false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:2,
+                autoplay:true,
+            },
+            600:{
+                items:3,
+                autoplay:true,
+            },
+            1000:{
+                items:6,
+                autoplay:false,
+            }
+        }
+    });
+
+    // === End owl-carousel === //
+
+
+    // magnificPopup
+    $('.gallery').magnificPopup({
+        delegate: '.popimg',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+    // countUp
+    $('.numbers .count').countUp({
+        delay: 10,
+        time: 1500
+    });
+
+});
+
+
+// === window When Loading === //
+
+$(window).on("load",function (){
+	//*** PRELOADER ***//
+		  $('#status').fadeOut(); // will first fade out the loading animation 
+		  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+		  $('body').delay(350).css({'overflow':'visible'});
+
+    var wind = $(window);
+
+     
+		
+    // stellar
+    wind.stellar();
+
+
+    // isotope
+    $('.gallery').isotope({
+      // options
+      itemSelector: '.items'
+    });
+
+    var $gallery = $('.gallery').isotope({
+      // options
+    });
+
+    // filter items on button click
+    $('.filtering').on( 'click', 'span', function() {
+
+        var filterValue = $(this).attr('data-filter');
+
+        $gallery.isotope({ filter: filterValue });
+
+    });
+
+    $('.filtering').on( 'click', 'span', function() {
+
+        $(this).addClass('active').siblings().removeClass('active');
+
+    });
+
+
+    // contact form validator
+    $('#contact-form').validator();
+
+    $('#contact-form').on('submit', function (e) {
+        if (!e.isDefaultPrevented()) {
+            var url = "contact.php";
+
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data)
+                {
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
+
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    if (messageAlert && messageText) {
+                        $('#contact-form').find('.messages').html(alertBox);
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    });
+
+});
+
+
